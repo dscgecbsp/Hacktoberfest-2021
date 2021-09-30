c9aeefa7ac739d4be7ce91042ce9743d22c0ce7e
Uditangshu Dey <67943382+uditangshu2000dey@users.noreply.github.com>
2021-10-01 04:03:12 +0530

    LargestRectangleInHistogram
    Adding a program on Largest Rectangle In Histogram

---
+++ LargestRectangleInHistogram.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+
+public class LargestRectangleInHistogram
+{
+    public int largestRectangleArea(int[] hist) {
+        int n = hist.length, maxArea = 0;
+        Stack<Integer>stack = new Stack<>();
+        int i = 0;
+        while(i < n){
+            if(stack.isEmpty() || hist[i] >= hist[stack.peek()])
+                stack.push(i++);
+            else{
+                int top = stack.peek();
+                stack.pop();
+                int currArea = hist[top]*(stack.isEmpty()?i:i-stack.peek()-1);
+                maxArea = Math.max(maxArea, currArea);
+            }
+        }
+        while(!stack.isEmpty()){
+            int top = stack.peek();
+            stack.pop();
+            int currArea = hist[top]*(stack.isEmpty()?i:i-stack.peek()-1);
+            maxArea = Math.max(maxArea, currArea);
+        }
+        return maxArea;
+    }
+}
