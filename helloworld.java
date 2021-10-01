c6d07a6e6e7337cd469c2088c9c1ddac5f72cf7f
Chaitanya Yadav <77014258+cy2000yadav@users.noreply.github.com>
2021-10-01 23:17:21 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,8 @@
-public class helloworld{
-public static void main(String args[])
-{
-System.out.println("Hello World");
-}
+public class HelloWorld {
+
+    public static void main(String[] args) {
+     
+        System.out.println("Hello, World");
+    }
+
 }
