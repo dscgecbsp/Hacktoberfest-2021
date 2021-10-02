24aadf9362c8483aff33496e6b00699d064eaea6
Mohammad Rafik Tadavi <72220094+mdrafikrft@users.noreply.github.com>
2021-10-02 14:50:28 +0530

    Arranged the code in systematic manner

--- BinSearch.c
+++ BinSearch.c
@@ -3,38 +3,41 @@
 void readArr(int[],int);
 int BinSearch(int[],int,int,int);
 void main()
-{
-    int A[20],n,key,i,x;
-    printf("Enter the number of elements: ");
-    scanf("%d",&n);
-    readArr(A,n);
-    printf("Enter the element to search: ");
-    scanf("%d",&key);
-    x=BinSearch(A,0,n-1,key);
-    if(x==-1)
-        printf("Element not found");
-    else
-        printf("Element found at position %d",x+1);
-}
-void readArr(int A[],int n)
-{
-    int i;
-    printf("Enter %d number of elements: ",n);
-    for(i=0;i<n;i++)
-        scanf("%d",&A[i]);
-}
-int BinSearch(int A[],int lwr,int upr,int key)
-{
-    int mid;
-    while(lwr<=upr)
     {
-        mid=(lwr+upr)/2;
-        if(A[mid]==key)
-            return mid;
-        else if(A[mid]<key)
-            lwr=mid+1;
-        else
-            upr=mid-1;
+        int A[20],n,key,i,x;
+        printf("Enter the number of elements: ");
+        scanf("%d",&n);
+        readArr(A,n);
+        printf("Enter the element to search: ");
+        scanf("%d",&key);
+        x=BinSearch(A,0,n-1,key);
+            if(x==-1)
+                printf("Element not found");
+            else
+                printf("Element found at position %d",x+1);
     }
-    return -1;
+            
+
+    void readArr(int A[],int n)
+        {
+            int i;
+            printf("Enter %d number of elements: ",n);
+            for(i=0;i<n;i++)
+            scanf("%d",&A[i]);
+        }
+    int BinSearch(int A[],int lwr,int upr,int key)
+        {
+            int mid;
+            while(lwr<=upr)
+                {
+                    mid=(lwr+upr)/2;
+                    if(A[mid]==key)
+                        return mid;
+                   else 
+                    if(A[mid]<key)
+                        lwr=mid+1;
+                   else
+                        upr=mid-1;
+                 }
+       return -1;
 }
