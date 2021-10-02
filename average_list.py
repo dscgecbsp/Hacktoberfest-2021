9df6b6981a0cc3061606c028d260973fc6d9a05f
Rronny01 <72240947+Rronny01@users.noreply.github.com>
2021-10-02 09:08:40 +0530

    Update average_list.py

--- average_list.py
+++ average_list.py
@@ -1,9 +1,12 @@
-numlist = list()                # create an empty list
+
+# Problem: Calculate average of numbers in the list from user input.
+
+numlist = list()                        # create an empty list
 while (True):
     inp = input('Enter a number: ')
-    if inp == 'done': break
+    if inp == 'done': break             # exit loop and calculate avg when you enter'done'
     value = float(inp)
-    numlist.append(value)       # append values in numlist
+    numlist.append(value)               # append values in numlist
 
 average = sum(numlist) / len(numlist)
 print('Average:', average)
