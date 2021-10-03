ab6043afff8ddde8fda41d3c81b82808a05d7e84
Raj Kamal Shakya <54174560+9759176595@users.noreply.github.com>
2021-10-03 07:10:26 +0530

    Create DEU.cpp

---
+++ DEU.cpp
@@ -0,0 +1,16 @@
+void make_set(int v) {
+    parent[v] = v;
+}
+
+int find_set(int v) {
+    if (v == parent[v])
+        return v;
+    return find_set(parent[v]);
+}
+
+void union_sets(int a, int b) {
+    a = find_set(a);
+    b = find_set(b);
+    if (a != b)
+        parent[b] = a;
+}
