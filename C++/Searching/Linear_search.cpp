604219238f8eca9fb31b7f65ac0f0efd4e814cf1
Snehil Saxena <91454963+snehilsaxena6248@users.noreply.github.com>
2021-10-03 06:54:54 +0530

    Update Linear_search.cpp

--- C++/Searching/Linear_search.cpp
+++ C++/Searching/Linear_search.cpp
@@ -1,39 +1,26 @@
-/*
-Problem statement: Search a element in a given array
-Input:
-5
-2 3 1 4 6
-1
-Output:
-Element found at index: 2
-
-*/
-
-// Linear Search in C++
-
 #include <iostream>
 using namespace std;
-
-int search(int array[], int n, int x) {
-
-  // Going through array sequencially
-  for (int i = 0; i < n; i++)
-    if (array[i] == x)
-      return i;
-  return -1;
+ 
+int search(int arr[], int n, int x)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        if (arr[i] == x)
+            return i;
+    return -1;
 }
-
-int main() {
-  int n;
-  cin>>n;
-  int array[n];
-  for(int i =0;i<n;i++){
-    cin>>array[i];
-  }
-  cout<<"Enter Element to search\n";
-  int x;
-  cin>>x;
-  int result = search(array, n, x);
-
-  (result == -1) ? cout << "Element not found" : cout << "Element found at index: " << result;
+ 
+// Driver code
+int main(void)
+{
+    int arr[] = { 2, 3, 4, 10, 40 };
+    int x = 10;
+    int n = sizeof(arr) / sizeof(arr[0]);
+   
+    // Function call
+    int result = search(arr, n, x);
+    (result == -1)
+        ? cout << "Element is not present in array"
+        : cout << "Element is present at index " << result;
+    return 0;
 }
