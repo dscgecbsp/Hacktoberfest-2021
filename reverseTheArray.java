6a68d68bb003576631d327d3a8679acfb13f36ce
Himanijindal <himani0422.cse19@chitkara.edu.in>
2021-10-04 17:02:57 +0530

    reverse

---
+++ reverseTheArray.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class reverseTheArray {
+    public static void reverse(int a[], int start, int end) {
+
+        int temp;
+        if (start > end) {
+            return;
+        }
+        temp = a[start];
+        a[start] = a[end];
+        a[end] = temp;
+        reverse(a, start + 1, end - 1);
+    }
+
+    public static void printArray(int a[]) {
+        for (int i = 0; i < a.length; i++)
+            System.out.print(a[i] + " ");
+        System.out.println("");
+    }
+
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int a[] = new int[n];
+        for (int i = 0; i < n; i++)
+            a[i] = sc.nextInt();
+        printArray(a);
+        reverse(a, 0, n - 1);
+        System.out.println("Reversed array is ");
+        printArray(a);
+    }
+}
