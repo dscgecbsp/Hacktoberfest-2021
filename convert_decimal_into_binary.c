d20c749f32c024bae465442978070738eafa7313
Ravindu007 <ravindu0504@gmail.com>
2021-10-04 22:06:29 +0530

    convert_decimal_inti_binary

---
+++ convert_decimal_into_binary.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void binary(int n);
+int sizeofthe_requiredArray(int n);
+
+int main()
+{
+    //getting a decimal input from the user
+    int number;
+    printf("Enter a decimal number: ");
+    scanf("%d", &number);
+
+    binary(number);
+    return 0;
+}
+
+void binary(int n){  //n =50
+
+    //finding the size of the array == how many times the number divide by 2
+    int size = sizeofthe_requiredArray(n); //k = 6
+
+
+
+    int binaryArray[size];
+
+    // array starting index
+    int i=0;
+
+    while(n!=0){
+       binaryArray[i] = n%2;
+       n = n/2;
+       i++;
+   }
+
+
+   for(int j=size; j>=0; j--){
+      printf("%d",binaryArray[j]);
+   }
+
+}
+
+int sizeofthe_requiredArray(int n){ // n=50
+
+    int k = 0;
+    while(n!=0){
+       n = n/2;
+       k++;
+    }
+
+    return k-1;
+}
