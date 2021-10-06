339ec333faa364112bab534426f6d354773f93b1
StutiSharma27 <92028077+StutiSharma27@users.noreply.github.com>
2021-10-06 20:44:11 +0530

    Create Inserting And Displaying Elements In Singly Linked List.cpp
    This Program is for inserting elements in a singly linked list and for displaying it.

---
+++ Inserting And Displaying Elements In Singly Linked List.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <malloc.h>
+using namespace std;
+
+struct node
+{
+    int data;
+    node* next;
+};
+
+struct node* head= NULL;
+
+//Function to insert data in linked list 
+void insert(int data)
+{
+    struct node *ptr= (node*)malloc(sizeof(struct node));
+    ptr->data=data;
+    ptr->next=head;
+    head=ptr;
+    
+}
+//Function to Display the entered linked list 
+void display()
+{
+    struct node *temp;
+    temp=head;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+}
+
+int main()
+{
+    int n,j;
+    cout<<"Enter the total number of elements you want to insert in the linked list : ";  
+    cin>>n;
+    cout<<"Enter the elements : \n";
+    for(int i=0;i<n;i++)
+    {
+        cin>>j;
+        insert(j);
+    }
+    cout<<"\nDisplaying the linked list : ";
+    display();
+    
+    return 0;
+}
