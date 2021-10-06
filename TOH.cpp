ccd65807546bf699cee45fd12200d910c5aa8f93
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-10-06 22:14:06 +0530

    Update TOH.cpp

--- TOH.cpp
+++ TOH.cpp
@@ -1,3 +1,10 @@
+/*Tower of Hanoi */
+
+/*
+This is a mathematical puzzle where we have three rods and n disks. The objective of the puzzle is to move the entire stack to another rod, obeying 2 rules: 
+(i) Only one disk can be moved at a time. (ii) No bigger disk may be placed on top of a smaller disk.
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
