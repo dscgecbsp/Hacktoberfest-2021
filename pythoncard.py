51a64f17012ed6ec978df70b7bc687d629072315
abbhardwaj01 <48086154+abbhardwaj01@users.noreply.github.com>
2021-10-06 12:33:00 +0530

    pythoncard.py
    game of rowlii card

---
+++ pythoncard.py
@@ -0,0 +1,42 @@
+class Card:
+    suits = ["spades",
+             "hearts",
+             "diamonds",
+             "clubs"]
+    
+    values = [None, None,"2", "3",
+              "4", "5", "6", "7",
+              "8", "9", "10",
+              "Jack", "Queen",
+              "King", "Ace"]
+
+    def __init__(self, v, s):
+        """suit + value are ints"""
+        self.value = v
+        self.suit = s
+
+    def __lt__(self, c2):
+        if self.value < c2.value:
+            return True
+        if self.value == c2.value:
+            if self.suit < c2.suit:
+                return True
+            else:
+                return False
+        return False
+
+    def __gt__(self, c2):
+        if self.value > c2.value:
+            return True
+        if self.value == c2.value:
+            if self.suit > c2.suit:
+                return True
+            else:
+                return False
+        return False
+
+    def __repr__(self):
+        v = self.values[self.value] +\
+            " of " + \
+            self.suits[self.suit]
+        return v
