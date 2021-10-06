066233d9996b067b1bdd6a2b0674ba4b800235ef
Eishita Gupta <eg7409@srmist.edu.in>
2021-10-06 21:55:04 +0530

    N-Queen Code added

--- N_Queen.cpp
+++ N_Queen.cpp
@@ -1,3 +1,35 @@
+/*
+N-Queens
+
+The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack each other.
+
+Given an integer n, return all distinct solutions to the n-queens puzzle. You may return the answer in any order.
+
+Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space, respectively.
+
+
+Example 1:
+
+Input: n = 4
+Output: [[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
+Explanation: There exist two distinct solutions to the 4-queens puzzle as shown above
+
+
+Example 2:
+
+Input: n = 1
+Output: [["Q"]]
+
+
+Constraints:
+
+1 <= n <= 9
+
+https://leetcode.com/problems/n-queens/
+
+*/
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
