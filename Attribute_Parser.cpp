2a74eccab165de10d169b8cf5d746c1eccee0dfc
Shivaminc <54238581+Shivaminc@users.noreply.github.com>
2021-10-06 21:26:35 +0530

    Update Attribute_Parser.cpp

--- Attribute_Parser.cpp
+++ Attribute_Parser.cpp
@@ -1,3 +1,39 @@
+Q. This challenge works with a custom-designed markup language HRML. In HRML, each element consists of a starting and ending tag, and there are attributes associated with each tag. Only starting tags can have attributes. We can call an attribute by referencing the tag, followed by a tilde, '~' and the name of the attribute. The tags may also be nested.
+
+The opening tags follow the format:
+
+<tag-name attribute1-name = "value1" attribute2-name = "value2" ...>
+
+The closing tags follow the format:
+
+</tag-name>
+
+The attributes are referenced as:
+
+tag1~value  
+tag1.tag2~name
+
+Given the source code in HRML format consisting of N lines, answer Q queries. For each query, 
+* print the value of the attribute specified. 
+* Print "Not Found!" if the attribute does not exist.
+
+Sample Input
+
+4 3
+<tag1 value = "HelloWorld">
+<tag2 name = "Name1">
+</tag2>
+</tag1>
+tag1.tag2~name
+tag1~name
+tag1~value
+
+Sample Output
+
+Name1
+Not Found!
+HelloWorld    
+    
 #include <iostream>
 #include <map>
 #include <sstream>
