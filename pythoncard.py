c07897378a271713bc73ec995699a1894fceb916
abbhardwaj01 <48086154+abbhardwaj01@users.noreply.github.com>
2021-10-07 13:00:10 +0530

    Update pythoncard.py
    This is python card game. The highest card combination is the winner. see code for more

--- pythoncard.py
+++ pythoncard.py
@@ -1,3 +1,13 @@
+# CARD GAME: ONE player draws a card from the deck and the player with the highest card wins. 
+#  this card game by defining classes representing a card, a deck, a player and finally the game itself.
+# The suits are known as spades,hearts,diamond and clubs and the numbers as ranking wise
+# The elements of the first two indexes of the value tuple are None, 
+# so the strings in the tuple match the index they represent. So the string “2” in the tuple of values is at index 2.
+# For example, you create a 2 of hearts by creating a Card object and passing it the parameters 2 (for color) and 1.
+# The code for these magic methods can also handle whether the cards have the same value, 
+#for example, if both cards have a value of 10. If this happens, the methods use the value of the combinations to break the tie
+#The combinations are ranked in order of strength in the combination tuple – with the strongest combination last, and 
+# thus assigned to the highest index, and the less powerful combination to the lowest index
 class Card:
     suits = ["spades",
              "hearts",
