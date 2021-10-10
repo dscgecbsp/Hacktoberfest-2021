cb00c2acc370b671c059f940f0f471c33d2ce1cf
Kanwar Jot Parkash Singh <52690070+kanwarjotp@users.noreply.github.com>
2021-10-10 19:22:07 +0530

    Added Docs and 3 new Stories
    Included documentation in the python file as comments. Included 3 new stories in the "stories.json" file

--- MadLibs-Generator/mad_libs_generator.py
+++ MadLibs-Generator/mad_libs_generator.py
@@ -1,30 +1,41 @@
 import json
 
-wrds = []
-
-type_libs = {
-    "1" : "adjective",
-    "2" : "noun",
-    "3" : "verb",
-    "4" : "relative's name",
-    "5" : "part of the body",
-    "6" : "plural noun",
-    "7" : "verb ending in 'ing'",
-    "8" : "room in a house",
-    "9" : "type of bird"
+# creating a dictionary to inform user of type of words to be entered as numbers(keys) correspond in stories
+type_of_words = {
+    "1": "adjective",
+    "2": "noun",
+    "3": "verb",
+    "4": "relative's name",
+    "5": "part of the body",
+    "6": "plural noun",
+    "7": "verb ending in 'ing'",
+    "8": "room in a house",
+    "9": "type of bird"
 }
 
-with open("stories.json",) as f:
-    data = json.load(f)
+input_words = []
 
-story_num = input("Pick a story from 1 to 10")
+# saving the stories and related information in a separate variable
+f = open("MadLibs-Generator/stories.json", "r")
+data = json.loads(f.read())
+f.close()
+
+# asking user's choice of story
+story_num = input("Pick a story from 1 to 5")
 
 story = (data["stories"])[story_num]
-num_libs = (data["num_libs"])[story_num]
 
+# loop over the story to take user input for words
 for i in story:
     if i.isdigit():
-        wrds.append(input("Enter a " + type_libs[i]))
+        input_words.append(input("Enter a " + type_of_words[i]))
 
-
-print(wrds)
+# introducing count variable to access words from "input_words" list
+count = 0
+# loop to print the story in the console
+for i in story:
+    if i.isdigit():
+        print(input_words[count], end='')
+        count += 1
+        continue
+    print(i, end='')
