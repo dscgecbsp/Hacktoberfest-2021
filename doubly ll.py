25e7a9a20fefe30f0ffd4e214208e46664ee22fc
Hellsgate96 <50798361+Hellsgate96@users.noreply.github.com>
2021-10-13 18:13:12 +0530

    Create doubly ll.py

---
+++ doubly ll.py
@@ -0,0 +1,10 @@
+/* Node of a doubly linked list */
+class Node
+{
+	public:
+	int data;
+	Node* next; // Pointer to next node in DLL
+	Node* prev; // Pointer to previous node in DLL
+};
+
+// This code is contributed by shivanisinghss2110
