9226eccfffe903c835a8d8a5891dba3779e81f62
Hritik Maheshwari <60053891+CRAZYGEEKS04@users.noreply.github.com>
2022-10-02 00:17:44 +0530

    Create SumAndDifferencesOfTwoNumber.cpp
    i made a solution in c++ so different user get it.

---
+++ HackerRank-C-main/SumAndDifferencesOfTwoNumber.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	
+    int a,b;
+    float c,d;
+    cin>>a>>b;
+    cin>>c>>b;
+    cout<<a+b<<a-b;
+    cout<<c+d<<c-d;
+    return 0;
+}
