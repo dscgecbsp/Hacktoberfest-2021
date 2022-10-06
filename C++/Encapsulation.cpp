3392c5b236576f2df6b74d8a65249d2ca282cc08
shahparam11 <103264744+shahparam11@users.noreply.github.com>
2022-10-06 10:27:11 +0530

    Create Encapsulation.cpp

---
+++ C++/Encapsulation.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Employee {
+  private:
+    // Private attribute
+    int salary;
+
+  public:
+    // Setter
+    void setSalary(int s) {
+      salary = s;
+    }
+    // Getter
+    int getSalary() {
+      return salary;
+    }
+};
+
+int main() {
+  Employee myObj;
+  myObj.setSalary(50000);
+  cout << myObj.getSalary();
+  return 0;
+}
