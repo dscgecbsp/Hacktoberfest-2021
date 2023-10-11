41effc3db6c2cd2268d368301b9b8e5575e07f19
Keshihan Ilamuruganathan <58644214+keshihan019@users.noreply.github.com>
2023-10-11 09:17:00 +0530

    Update Improved_Bubble_Sort.cpp

--- Improved_Bubble_Sort.cpp
+++ Improved_Bubble_Sort.cpp
@@ -1,33 +1,33 @@
-//Program : To sort a given array using an improved version of bubble sort which has a better time complexity.
 #include<iostream>
 using namespace std;
-int main(void){
+
+int main() {
     int n;
-    cout<<"Enter Size of array: ";
-    cin>>n;
+    cout << "Enter Size of array: ";
+    cin >> n;
     int arr[n];
-    cout<<"Enter elements in the array: ";
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+    cout << "Enter elements in the array: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
     }
-    bool flag=false; //a flag that is set if an exchange is made after an entire pass over the array
-    for(int x=0; x<n; x++)
- {
-     flag=false;
-    for(int y=0; y<n-1; y++)
-    {
-        if(arr[y]>arr[y+1])
-        {
-            swap(arr[y],arr[y+1]);
-            flag = true;
+    
+    bool flag = true;  // Initialize flag to true
+    
+    for (int x = 0; x < n && flag; x++) {  // Update loop condition
+        flag = false;  // Reset flag for this iteration
+        for (int y = 0; y < n - 1; y++) {
+            if (arr[y] > arr[y + 1]) {
+                swap(arr[y], arr[y + 1]);
+                flag = true;  // Set flag if a swap occurs
+            }
         }
     }
-    if(!flag){
-        break;
-        }
+    
+    cout << "Elements in array after sorting: " << endl;
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
     }
-    cout<<"Elements in array after sorting: "<<endl;
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
-    }
+    cout << endl;
+
+    return 0;
 }
